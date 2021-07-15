98a7fcd99c8ca008d890bd9ce7e6ba9a0a5f1215
log2hong <hrocky125@snu.ac.kr>
2021-07-15 18:03:48 +0900

    A+B - 3

---
+++ 10950.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int tc, a, b;
+    cin >> tc;
+    for(int i = 0; i < tc; i++){
+        cin >> a >> b;
+        cout << a + b << endl;
+    }
+    return 0;
+}
