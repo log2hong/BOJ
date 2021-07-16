040dc7408f63a12ccdc0ea7031ce9d6d7c48be37
log2hong <hrocky125@snu.ac.kr>
2021-07-16 14:56:38 +0900

    나머지

---
+++ 3052.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main(){
+    int tmp;
+    set<int> s;
+
+    for(int i = 0; i < 10; i++){
+        cin >> tmp;
+        s.insert(tmp % 42);
+    }
+    cout << s.size() << endl;
+    return 0;
+}
