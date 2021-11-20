306d3c1062a261f35b801e18a27b74568f9fcb54
Rokgi Hong <hrocky125@snu.ac.kr>
2021-11-20 17:06:40 +0900

    블랙잭

---
+++ 2798.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n, m;
+    cin >> n >> m;
+    int *arr = new int[n];
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    
+    int ans = 0, temp;
+    for(int i = 0; i < n - 2; i++){
+        for(int j = i + 1; j < n - 1; j++){
+            for(int k = j + 1; k < n; k++){
+                temp = arr[i] + arr[j] + arr[k];
+                if(m < temp) continue;
+                if(m - ans > m - temp){
+                    ans = temp;
+                }
+            }
+        }
+    }
+    cout << ans << endl;
+    
+    delete[] arr;
+    return 0;
+}
