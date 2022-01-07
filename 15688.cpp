a5dfb1587f896aa7d2b0fccfa786a95a389a275d
log2hong <hrocky125@snu.ac.kr>
2022-01-08 00:54:00 +0900

    수 정렬하기 5

---
+++ 15688.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n, tmp;
+    vector<int> v;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        cin >> tmp;
+        v.push_back(tmp);
+    }
+    sort(v.begin(), v.end());
+    for(vector<int>::iterator it = v.begin(); it != v.end(); it++){
+        cout << *it << '\n';
+    }
+    return 0;
+}
