e71eb01cca0e0e4258865a5b59e59fa75ff815d2
log2hong <hrocky125@snu.ac.kr>
2022-01-11 22:36:19 +0900

    2×n 타일링

---
+++ 11726.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+long long int d[1001] = {1, 1};
+
+long long int Fibonacci(int n){
+    if(n == 0) return 1;
+    if(n == 1) return 1;
+    if(d[n] != 0) return d[n];
+    d[n] = (Fibonacci(n - 1) + Fibonacci(n - 2)) % 10007;
+    return d[n];
+}
+
+int main(){
+    int n;
+    cin >> n;
+    cout << Fibonacci(n) << endl;
+    return 0;
+}
