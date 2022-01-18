93bb5ac55504f1cb93e3f8ee8d992df817405b80
log2hong <hrocky125@snu.ac.kr>
2022-01-18 21:26:09 +0900

    연세대학교

---
+++ 15680.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    cout << (n ? "Leading the Way to the Future" : "YONSEI") << endl;
+    return 0;
+}
