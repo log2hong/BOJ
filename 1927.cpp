b5c7d9d5d0838ec9f6ffb65c2fa135ff6864df9b
Rokgi Hong <hrocky125@snu.ac.kr>
2022-01-21 19:29:55 +0900

    최소 힙

---
+++ 1927.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n;
+    priority_queue<int> Q;
+    
+    cin >> n;
+    while(n--){
+        int x;
+        cin >> x;
+        if(x != 0) Q.push(-x);
+        else{
+            if(Q.empty()) cout << "0" << '\n';
+            else{
+                cout << -Q.top() << '\n';
+                Q.pop();
+            }
+        }
+    }
+    return 0;
+}
