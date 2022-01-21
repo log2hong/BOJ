0b8bc731beb880cf4164e820b0f3f34b44cb7c6a
Rokgi Hong <hrocky125@snu.ac.kr>
2022-01-21 19:20:56 +0900

    하노이 탑 이동 순서

--- 11729.cpp
+++ 11729.cpp
@@ -3,22 +3,23 @@
 using namespace std;
 
 void Hanoi(int n, int a, int b){
-    if(n == 0) return;
-
+    if(n == 1){
+        cout << a << ' ' << b << '\n';
+        return;
+    }
     Hanoi(n - 1, a, 6 - a - b);
-    cout << a << " " << b << '\n';
+    cout << a << ' ' << b << '\n';
     Hanoi(n - 1, 6 - a - b, b);
 }
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(0);
-    cout.tie(0);
 
     int n;
     cin >> n;
 
-    cout << pow(2, n) - 1 << '\n';
+    cout << (1 << n) - 1 << '\n';
     Hanoi(n, 1, 3);
 
     return 0;
