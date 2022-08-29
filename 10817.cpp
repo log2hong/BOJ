ea2ee2903868bb264ab18bd73c5caaacde010bc0
Rokgi Hong <hrocky125@snu.ac.kr>
2022-08-29 20:11:23 +0900

    세 수

---
+++ 10817.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    int arr[2];
+    for(int i = 0; i < 3; i++) cin >> arr[i];
+    sort(arr, arr + 3);
+    cout << arr[1] << endl;
+    return 0;
+}
