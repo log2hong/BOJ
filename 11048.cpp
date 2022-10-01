85c175a5baae2b4285012af445aebc79b802e455
Rokgi Hong <hrocky125@snu.ac.kr>
2022-10-01 20:52:44 +0900

    이동하기

---
+++ 11048.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int arr[1001][1001];
+int dp[1001][1001];
+
+int main(){
+    int n, m;
+    cin >> n >> m;
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= m; j++){
+            cin >> arr[i][j];
+        }
+    }
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= m; j++){
+            dp[i][j] = max(max(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]) + arr[i][j];
+        }
+    }
+    cout << dp[n][m] << endl;
+    return 0;
+}
