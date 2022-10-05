a5e1e8b617cdd0476a6cb9e104b9991cd9d9f1f7
Rokgi Hong <hrocky125@snu.ac.kr>
2022-10-05 15:56:01 +0900

    Baseball

--- 10214.py
+++ 10214.py
@@ -3,9 +3,9 @@ tc = int(input())
 for i in range(tc):
     y, k = 0, 0
     for j in range(9):
-        x, y = map(int, input().split())
-        y += x
-        k += y
+        n, m = map(int, input().split())
+        y += n
+        k += m
     if y > k:
         print("Yonsei")
     elif y == k:
