211571397560c72ef40bc339fb92f21d5496d6c7
Rokgi Hong <hrocky125@snu.ac.kr>
2022-10-05 17:22:01 +0900

    어린 왕자

--- 1004.py
+++ 1004.py
@@ -6,8 +6,8 @@ for i in range(tc):
     ans = 0
     for i in range(n):
         cx, cy, r = map(int, input().split())
-        d1 = (((x1 - cx) ** 2) + ((y1 - cy) ** 2))
-        d2 = (((x2 - cx) ** 2) + ((y2 - cy) ** 2))
+        d1 = (x1 - cx) ** 2 + (y1 - cy) ** 2
+        d2 = (x2 - cx) ** 2 + (y2 - cy) ** 2
         if (d1 < r ** 2 and d2 > r ** 2) or (d1 > r ** 2 and d2 < r ** 2):
             ans += 1
     print(ans)
