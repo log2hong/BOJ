bf85f4a15b90608e752ff17271021593649aa2f1
Rokgi Hong <hrocky125@snu.ac.kr>
2023-05-03 17:55:36 +0900

    오등큰수

---
+++ 17299.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+const int MAX_N = 1000000;
+int arr[MAX_N + 1], ans[MAX_N + 1], cnt[MAX_N + 1];
+
+int main(){
+    int n;
+    cin >> n;
+    
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+        cnt[arr[i]]++;
+    }
+    
+    stack<int> st;
+    for(int i = 0; i < n; i++){
+        while(!st.empty() && cnt[arr[st.top()]] < cnt[arr[i]]){
+            ans[st.top()] = arr[i];
+            st.pop();
+        }
+        st.push(i);
+    }
+    while(!st.empty()){
+        ans[st.top()] = -1;
+        st.pop();
+    }
+
+    for(int i = 0; i < n; i++)
+        cout << ans[i] << " ";
+    
+    return 0;
+}
