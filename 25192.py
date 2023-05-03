267d99a56f7dbf3d0bf03dbe45bcb9ad62bff2f3
Rokgi Hong <hrocky125@snu.ac.kr>
2023-05-03 15:21:19 +0900

    인사성 밝은 곰곰이

---
+++ 25192.py
@@ -0,0 +1,23 @@
+n = int(input())
+_ = str(input())
+
+cnt = 0
+dict = {}
+
+for _ in range(n - 1):
+    s = str(input())
+    if s == "ENTER":
+        for key, value in dict.items():
+            if value == 1:  
+                cnt += 1 
+        dict = {} 
+        continue
+
+    if s not in dict:
+        dict[s] = 1 
+
+for _, value in dict.items():
+    if value == 1:
+        cnt += 1
+
+print(cnt)
