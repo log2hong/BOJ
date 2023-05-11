357c7239d99774741af0fbdc4abaa4034e8a231d
Rokgi Hong <hrocky125@snu.ac.kr>
2023-05-11 14:39:18 +0900

    가장 긴 증가하는 부분 수열 4

---
+++ 14002.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    int* arr = new int[n];
+    int* lis = new int[n];
+    int* index = new int[n];
+
+    int idx = 0;
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+        if(idx == 0){
+            index[i] = idx;
+            lis[idx] = arr[i];
+            idx++;
+        }
+        else{
+            if(lis[idx - 1] < arr[i]){
+                index[i] = idx;
+                lis[idx] = arr[i];
+                idx++;
+            }
+            else{
+                index[i] = lower_bound(lis, lis + idx, arr[i]) - lis;
+                lis[index[i]] = arr[i];
+            }
+        }    
+    }
+    
+    cout << idx << endl;
+
+    int* ans = new int[idx];
+    int k = 0;
+    for(int i = n - 1; i >= 0; i--){
+        if(idx == index[i] + 1){
+            ans[k++] = arr[i];
+            idx--;
+        }
+    }
+    for(int i = k - 1; i >= 0; i--){
+        cout << ans[i] << ' ';
+    }
+
+    delete[] arr, lis, index, ans;
+    return 0;
+}
