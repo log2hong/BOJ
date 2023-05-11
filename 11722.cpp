ee865577589930060e1ce09a5062d68240d85740
Rokgi Hong <hrocky125@snu.ac.kr>
2023-05-11 14:45:07 +0900

    가장 긴 감소하는 부분 수열

---
+++ 11722.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    int* arr = new int[n];
+    int* lis = new int[n];
+
+    for(int i = n - 1; i >= 0; i--){
+        cin >> arr[i];
+    }
+
+    int idx = 0;
+    for(int i = 0; i < n; i++){
+        if(idx == 0){
+            lis[idx] = arr[i];
+            idx++;
+        }
+        else{
+            if(lis[idx - 1] < arr[i]){
+                lis[idx] = arr[i];
+                idx++;
+            }
+            else{
+                lis[lower_bound(lis, lis + idx, arr[i]) - lis] = arr[i];
+            }
+        }    
+    }
+
+    cout << idx << endl;
+
+    delete[] arr, lis;
+    return 0;
+}
